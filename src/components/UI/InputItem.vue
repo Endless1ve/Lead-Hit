89b81c6e8154b60e3a837f8b8fd72e24408c022b
Artem Tulupov <stalin.dignitas70@gmail.com>
2024-04-10 16:06:33 +0300

    feat(custom): added UI input

---
+++ src/components/UI/InputItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <input class="input" type="text" :placeholder="placeholder" />
+</template>
+
+<script>
+  export default {
+    props: {
+      placeholder: {
+        type: String,
+        required: true,
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  .input {
+    width: 100%;
+    border: $form-input-border-size $form-input-border-color solid;
+    border-radius: $border-radius;
+    padding: 10px 10px;
+    outline: none;
+    font-size: 15px;
+  }
+</style>
