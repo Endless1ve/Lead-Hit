19746bf578c3c84da80edf6b9de0b47fb8b39cba
Artem Tulupov <stalin.dignitas70@gmail.com>
2024-04-10 16:04:44 +0300

    feat(custom): added UI form button

---
+++ src/components/UI/ButtonItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="button"><slot></slot></button>
+</template>
+
+<style lang="scss" scoped>
+  .button {
+    width: 100%;
+    border: none;
+    border-radius: $border-radius;
+    padding: 10px 0;
+    color: $main-light-color;
+    font-size: 15px;
+    background-color: $accent-color;
+    cursor: pointer;
+
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+</style>
