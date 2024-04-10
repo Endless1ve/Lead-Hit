76e96b50d6debf2df8d0880d6672e3f1e40b4d3a
Artem Tulupov <stalin.dignitas70@gmail.com>
2024-04-10 16:16:20 +0300

    add auth states

--- src/store/modules/AuthModule.js
+++ src/store/modules/AuthModule.js
@@ -1,4 +1,13 @@
 const AuthModule = {
+  state: () => ({
+    siteId: "",
+    isErrorVisible: false,
+    missLength: "id сайта должен содержать 24 символа",
+    incorrectValue: "некорректное значение",
+    serverError: "ошибка сервера",
+    errorText: "",
+    isLoading: false,
+  }),
   namespaced: true,
 };
 
