e9760b4d479c34d8e64d3297e68ddc864971015b
Artem Tulupov <stalin.dignitas70@gmail.com>
2024-04-10 15:56:49 +0300

    feat(custom): added variables
    Добавил файл с переменными и добавил их для загрузки во все файлы

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,11 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/Lead-Hit/" : "/",
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "@/assets/styles/variables.scss";`,
+      },
+    },
+  },
 });
